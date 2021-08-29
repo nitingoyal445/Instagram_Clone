d0d2e6139f55a2000425c4890dd45778086196e5
nitingoyal445 <nitingoyal445@gmail.com>
2021-08-30 00:35:32 +0530

    user crud operation complete

--- activity/backend/controller/userController.js
+++ activity/backend/controller/userController.js
@@ -1,16 +1,103 @@
 const connection = require("../model/db");
 const { v4: uuidv4 } = require('uuid');
 
-async function getAllUsers(){
-
+function getAllUsers(req, res){
+        const sql = `SELECT * FROM user_table`;
+        connection.query(sql, function(error, data){
+        if(error){
+            res.json({
+                message : "Failed to get all users",
+                error
+            })
+        }
+        else{
+            res.status(200).json({
+                message: " Got all users !!",
+                data
+            })
+        }
+    })
 }
-async function getUserById(){
 
+function getUserById(req, res){
+        const {id} = req.params;
+        const sql = `SELECT * FROM user_table WHERE uid = '${id}'`;
+        console.log(sql); 
+        connection.query(sql, function(error, data){
+            if(error){
+                res.json({
+                    message : "Failed to get user",
+                    error
+                })
+            }
+            else{
+                if(data.length){
+                    res.status(200).json({
+                        message:"Got user by id",
+                        data
+                    });
+                }
+                else{
+                    res.status(200).json({
+                        message : "No User Found !!"
+                    })
+                }
+            }
+        }) 
 }
-async function updateUserById(){
 
+function updateUserById(req, res){
+    const {id} = req.params;
+    const updateObject = req.body;
+    let sql = `UPDATE user_table SET `;
+    for(key in updateObject){
+        sql+= `${key} = '${updateObject[key]}',`
+    }
+    sql = sql.substring(0, sql.length-1);
+    sql += ` WHERE uid = '${id}'`;
+    // console.log(sql);
+    connection.query(sql, function(error, data){
+        if(error){
+            res.json({
+                message:"Failed to update",
+                error
+            })
+        }
+        else{
+            res.status(201).json({
+                message: "updated user !!",
+                data
+            })
+        }
+    })
 }
-async function deleteUserById(){
+
+
+function deleteUserById(req, res){
+    const {id} = req.params;
+    const sql = `DELETE FROM user_table WHERE uid='${id}'`;
+    connection.query(sql , function(error, data){
+            if(error){
+                res.json({
+                    message:"Failed to delete a user",
+                    error
+                })
+            }
+            else{
+                if(data.affectedRows){
+                    res.status(200).json({
+                        message: "Successfully deleted a user",
+                        data
+                    })
+                }
+                else{
+                    res.json({
+                        message: "No user found !!"
+                    })
+                }
+                
+            }
+        })
 
 }
 
